32a12e4b8ca08cd27feceafbdce7244de8bec4a2
karar <karar@karar.local>
2021-01-30 15:30:28 +0100

    RouteLocator added with dummy filters, hystrix curcuit breaker is added as sample, working filter with httpbin server

--- src/main/java/com/lilaksoft/digisellgateway/DigisellGatewayApplication.java
+++ src/main/java/com/lilaksoft/digisellgateway/DigisellGatewayApplication.java
@@ -2,12 +2,44 @@ package com.lilaksoft.digisellgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@EnableConfigurationProperties(UriConfiguration.class)
+@RestController
 public class DigisellGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DigisellGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+		String httpUri = uriConfiguration.getHttpbin();
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri(httpUri))
+				.route(p -> p
+						.host("*.hystrix.com")
+						.filters(f -> f
+								.hystrix(config -> config
+										.setName("mycmd")
+										.setFallbackUri("forward:/fallback")))
+						.uri(httpUri))
+				.build();
+	}
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
 	}
 
 }
---
+++ src/main/java/com/lilaksoft/digisellgateway/UriConfiguration.java
@@ -0,0 +1,17 @@
+package com.lilaksoft.digisellgateway;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties
+class UriConfiguration {
+
+  private String httpbin = "http://httpbin.org:80";
+
+  public String getHttpbin() {
+    return httpbin;
+  }
+
+  public void setHttpbin(String httpbin) {
+    this.httpbin = httpbin;
+  }
+}
