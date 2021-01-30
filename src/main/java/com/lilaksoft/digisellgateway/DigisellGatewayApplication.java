bcd64d73a0eafdce3a601b87dc2835659dde7742
karar <karar@karar.local>
2021-01-30 15:18:58 +0100

    initial version

---
+++ src/main/java/com/lilaksoft/digisellgateway/DigisellGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lilaksoft.digisellgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigisellGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigisellGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lilaksoft/digisellgateway/DigisellGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilaksoft.digisellgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigisellGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
